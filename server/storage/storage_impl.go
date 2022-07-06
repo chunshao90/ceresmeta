a8efd176afe20bb6d5d5b4e2b74089cd3ceef32c
baojinri <baojinri@163.com>
2022-07-06 16:36:41 +0800

    impl MetaStorage interface

--- server/storage/storage_impl.go
+++ server/storage/storage_impl.go
@@ -18,9 +18,14 @@
 package storage
 
 import (
+	"fmt"
+	"strings"
 	"time"
 
+	"github.com/CeresDB/ceresdbproto/pkg/metapb"
+	"github.com/CeresDB/ceresmeta/server/etcdutil"
 	clientv3 "go.etcd.io/etcd/client/v3"
+	"google.golang.org/protobuf/proto"
 )
 
 // StorageImpl is the base underlying storage endpoint for all other upper
@@ -44,4 +49,119 @@ func NewStorageImpl(
 func newEtcdStorage(client *clientv3.Client, rootPath string, requestTimeout time.Duration) *StorageImpl {
 	return NewStorageImpl(
 		NewEtcdKV(client, rootPath, requestTimeout))
+}
+
+func (si *StorageImpl) GetCluster(clusterId uint32, meta *metapb.Cluster) (bool, error) {
+	value, err := si.Get(fmt.Sprintf("%d", clusterId))
+	if err != nil || value == "" {
+		return false, err
+	}
+	err = proto.Unmarshal([]byte(value), meta)
+	if err != nil {
+		return false, etcdutil.ErrEtcdKVGet.WithCause(err)
+	}
+	return true, nil
+}
+
+func (si *StorageImpl) PutCluster(clusterId uint32, meta *metapb.Cluster) error {
+	value, err := proto.Marshal(meta)
+	if err != nil {
+		return etcdutil.ErrEtcdKVGet.WithCause(err)
+	}
+	return si.Put(fmt.Sprintf("%d", clusterId), string(value))
+}
+
+func (si *StorageImpl) GetClusterTopology(clusterId uint32, clusterMetaData *metapb.ClusterTopology) (bool, error) {
+	value, err := si.Get(fmt.Sprintf("%d", clusterId))
+	if err != nil || value == "" {
+		return false, err
+	}
+	err = proto.Unmarshal([]byte(value), clusterMetaData)
+	if err != nil {
+		return false, etcdutil.ErrEtcdKVGet.WithCause(err)
+	}
+	return true, nil
+}
+
+func (si *StorageImpl) PutClusterTopology(clusterId uint32, clusterMetaData *metapb.ClusterTopology) error {
+	value, err := proto.Marshal(clusterMetaData)
+	if err != nil {
+		return etcdutil.ErrEtcdKVGet.WithCause(err)
+	}
+	return si.Put(fmt.Sprintf("%d", clusterId), string(value))
+}
+
+func (si *StorageImpl) GetTables(clusterId uint32, schemaId uint32, tableId []uint64, table []*metapb.Table) (bool, error) {
+	for _, item := range tableId {
+		key := strings.Join([]string{fmt.Sprintf("%d", clusterId), fmt.Sprintf("%d", schemaId), fmt.Sprintf("%d", item)}, delimiter)
+		value, err := si.Get(key)
+		if err != nil || value == "" {
+			return false, err
+		}
+		var tableData *metapb.Table
+		err = proto.Unmarshal([]byte(value), tableData)
+		if err != nil {
+			return false, etcdutil.ErrEtcdKVGet.WithCause(err)
+		}
+		table = append(table, tableData)
+	}
+	return true, nil
+}
+
+func (si *StorageImpl) PutTables(clusterId uint32, schemaId uint32, tables []*metapb.Table) error {
+	for _, item := range tables {
+		key := strings.Join([]string{fmt.Sprintf("%d", clusterId), fmt.Sprintf("%d", schemaId), fmt.Sprintf("%d", item.Id)}, delimiter)
+		value, err := proto.Marshal(item)
+		if err != nil {
+			return etcdutil.ErrEtcdKVGet.WithCause(err)
+		}
+		err = si.Put(key, string(value))
+		if err != nil {
+			return err
+		}
+	}
+	return nil
+}
+
+func (si *StorageImpl) DeleteTables(clusterId uint32, schemaId uint32, tableIDs []uint64) (bool, error) {
+	for _, item := range tableIDs {
+		key := strings.Join([]string{fmt.Sprintf("%d", clusterId), fmt.Sprintf("%d", schemaId), fmt.Sprintf("%d", item)}, delimiter)
+		err := si.Delete(key)
+		if err != nil {
+			return false, err
+		}
+	}
+	return true, nil
+}
+
+func (si *StorageImpl) GetShardTopologies(clusterId uint32, shardId []uint32, shardTableInfo []*metapb.ShardTopology) (bool, error) {
+	for _, item := range shardId {
+		key := strings.Join([]string{fmt.Sprintf("%d", clusterId), fmt.Sprintf("%d", item)}, delimiter)
+		value, err := si.Get(key)
+		if err != nil || value == "" {
+			return false, err
+		}
+		var shardTableData *metapb.ShardTopology
+		err = proto.Unmarshal([]byte(value), shardTableData)
+		if err != nil {
+			return false, etcdutil.ErrEtcdKVGet.WithCause(err)
+		}
+		shardTableInfo = append(shardTableInfo, shardTableData)
+	}
+	return true, nil
+}
+
+func (si *StorageImpl) PutShardTopologies(clusterId uint32, shardId []uint32, shardTableInfo []*metapb.ShardTopology) error {
+	for index, item := range shardId {
+		key := strings.Join([]string{fmt.Sprintf("%d", clusterId), fmt.Sprintf("%d", item)}, delimiter)
+		value, err := proto.Marshal(shardTableInfo[index])
+		if err != nil {
+			return etcdutil.ErrEtcdKVGet.WithCause(err)
+		}
+		err = si.Put(key, string(value))
+		if err != nil {
+			return err
+		}
+	}
+	return nil
 }
